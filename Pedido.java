0bc2fa72975454e804874c06fe8c2e93f4ceb229
Brunoborgescarvalho <borgesbruno983@gmail.com>
2023-10-31 11:18:59 -0300

    first commit

---
+++ Pedido.java
@@ -0,0 +1,17 @@
+public class Pedido {
+    private int codigo;
+    private double valor;
+    public int getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    public double getValor() {
+        return valor;
+    }
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    
+}
